ce8756f7e3bdc3bd5b7cf3706b8fc40ed35687de
Justin Jeffery <justin.jeffery@ipfabric.io>
2021-12-16 10:18:40 -0500

    fix readme

--- src/automation.py
+++ src/automation.py
@@ -1,7 +1,12 @@
 from models import Event
+from ipfabric import IPFClient
+from config import settings
 
+IPF = IPFClient(base_url=settings.ipf_url, token=settings.ipf_token, verify=settings.ipf_verify)
 
 
 def process_event(event: Event):
     if event.type != 'snapshot' or event.action != 'discover' or event.status != 'completed':
         return
+    IPF.update()
+    IPF.snapshot_id = event.snapshot.id
